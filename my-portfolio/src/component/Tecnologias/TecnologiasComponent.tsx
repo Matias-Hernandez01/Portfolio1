24125116f2cc432cbd271ecdbdac50e07c3787ed
Matias Hernandez <matias2k00@gmail.com>
2023-07-12 11:23:10 -0300

    tecnologias component

---
+++ my-portfolio/src/component/Tecnologias/TecnologiasComponent.tsx
@@ -0,0 +1,53 @@
+import { Landing } from '../../view';
+import style from './TecnologiasComponent.module.css';
+const TecnologiasComponent = () => {
+  return (
+    <div className={style.mainContainer}>
+      <div className={style.landingContainer}>
+        <Landing />
+      </div>
+      <div className={style.skillContainer}>
+        <div>
+          <img />
+        </div>
+        {/* <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div>
+        <div>
+          <img src={} alt='image Skill' />
+        </div> */}
+      </div>
+    </div>
+  );
+};
+export default TecnologiasComponent;
